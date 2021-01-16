fefc572cbe6545555544fcd6082461212c310ed3
Sergey Goppikov <grey2k@gmail.com>
2021-01-17 01:20:10 +0300

    3/7 Completed

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,1 +1,22 @@
-# write your code here
+COMMAND_EXIT = '/exit'
+COMMAND_HELP = '/help'
+
+while True:
+    try:
+        user_input = input().strip()
+
+        if len(user_input) == 0:
+            continue
+    except EOFError:
+        continue
+
+    if user_input == COMMAND_EXIT:
+        break
+
+    if user_input == COMMAND_HELP:
+        print('The program calculates the sum of numbers')
+        continue
+
+    print(sum(map(int, [x for x in user_input.split() if x.strip() != ""])))
+
+print('Bye!')
